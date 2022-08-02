249e8edb1a1ceab313411cda5d7341661521c4a5
Matsilog <camataba@gmail.com>
2022-08-02 20:03:36 +0700

    added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../style/components/Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='navbar_container'>This is a Navbar</div>
+    
+  )
+}
+
+export default Navbar
