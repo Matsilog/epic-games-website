60b10946699f6b8070c7475318178f5bcc7d3f58
Matsilog <camataba@gmail.com>
2022-08-04 21:25:12 +0800

    Logo and Navbar  Enhancements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
 import '../style/components/Navbar.css'
+
+import Epicgameslogo from '../asset/image/Epic_logo.png'
 const Navbar = () => {
   return (
-    <div className='navbar_container'>This is a Navbar</div>
-    
+    <div className='navbar_container'>
+        <div classname='image  container'>
+          <img className='logo' src={Epicgameslogo} />
+        </div>
+        <ul className='navbar_list'>
+        <li>STORE</li>
+          <li>NEWS</li>
+          <li>FAQ</li>
+          <li>HELP</li>
+          <li>UNREAL ENGINE</li>
+          <div className='divider'></div>
+          <li>ABOUT EPIC</li>
+        </ul>
+     </div>
   )
 }
 
