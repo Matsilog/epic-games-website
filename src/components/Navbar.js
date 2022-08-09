18288ba819d0149bf6cbb0ed02d8523d61f69b8e
Matsilog <camataba@gmail.com>
2022-08-09 21:06:29 +0800

    Continued Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from "react";
 
-import '../style/components/Navbar.css'
+import "../style/components/Navbar.css";
 
-import Epicgameslogo from '../asset/image/Epic_logo.png'
+import Epicgameslogo from "../asset/image/Epic_logo.png";
 const Navbar = () => {
   return (
-    <div className='navbar_container'>
-        <div classname='image  container'>
-          <img className='logo' src={Epicgameslogo} />
-        </div>
-        <ul className='navbar_list'>
+    <div className="navbar_container">
+      <div classname="image  container">
+        <img className="logo" src={Epicgameslogo} />
+      </div>
+      <ul className="navbar_list">
         <li>STORE</li>
-          <li>NEWS</li>
-          <li>FAQ</li>
-          <li>HELP</li>
-          <li>UNREAL ENGINE</li>
-          <div className='divider'></div>
-          <li>ABOUT EPIC</li>
-        </ul>
-     </div>
-  )
-}
+        <li>NEWS</li>
+        <li>FAQ</li>
+        <li>HELP</li>
+        <li>UNREAL ENGINE</li>
+        <div className="divider"></div>
+        <li>ABOUT EPIC</li>
+      </ul>
+      <ul className="navbar_list2">
+        <li>world icon</li>
+        <li>username</li>
+        <li>get epic games</li>
+      </ul>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
